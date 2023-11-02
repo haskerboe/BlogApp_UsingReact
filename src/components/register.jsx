ec291507dc0b89f2b95ce51f181a97ac67b5a0cf
Roma <haskerboe@gmail.com>
2023-11-02 14:18:43 +0500

    API action register

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
 import { logo } from '../constants'
 import { Input } from '../ui'
+import { useDispatch, useSelector } from 'react-redux'
+import { registerUserStart } from '../slice/auth'
 
 const Register = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const dispatch = useDispatch()
+    const { isLoading } = useSelector(state => state.auth)
+    const registerHandler = e => {
+        e.preventDefault()
+        dispatch(registerUserStart())
+    }
     return (
         <div className='text-center mt-5'>
             <main className="form-signin w-25 m-auto">
@@ -16,7 +24,9 @@ const Register = () => {
                     <Input label={'Email Address'} state={email} setState={setEmail} />
                     <Input label={'Password'} state={password} setState={setPassword} type={'password'} />
 
-                    <button className="btn btn-primary w-100 py-2 mt-3" type="submit">Register</button>
+                    <button className="btn btn-primary w-100 py-2 mt-3" type="submit" onClick={registerHandler} disabled={isLoading}>
+                        {isLoading ? 'Loading...' : 'Register'}
+                    </button>
                 </form>
             </main>
         </div>
