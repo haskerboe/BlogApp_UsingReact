e5016f7ea0d4dee3b0cf873861f4187b1506072f
Roma <haskerboe@gmail.com>
2023-11-07 02:21:50 +0500

    Loader

---
+++ src/ui/loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <div class="spinner-border d-block mx-auto" role="status">
+            <span class="sr-only"></span>
+        </div>
+    )
+}
+
+export default Loader
