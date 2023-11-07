7840906697c432ee04ae8b57c7992cb7a5a5a64c
Roma <haskerboe@gmail.com>
2023-11-07 15:42:23 +0500

    Article detail. API

--- src/services/article.js
+++ src/services/article.js
@@ -4,6 +4,10 @@ const ArticleService = {
     async getArticles() {
         const { data } = await axios.get('/articles')
         return data
+    },
+    async getARticleDetail(slug) {
+        const { data } = await axios.get(`/articles/${slug}`)
+        return data
     }
 }
 
