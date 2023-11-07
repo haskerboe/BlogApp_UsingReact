3db9fd3803c68d0e795c991c81c1e88ed9c06d34
Roma <haskerboe@gmail.com>
2023-11-08 01:42:55 +0500

    disable eslint for useState []

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ const App = () => {
       getUser()
     }
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
     <div>
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -23,6 +23,7 @@ const Main = () => {
 
     useEffect(() => {
         getArticles()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
         <>
