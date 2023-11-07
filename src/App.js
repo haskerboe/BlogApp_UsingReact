bca2ba1a49764c67784cd74009146c45a36786e4
Roma <haskerboe@gmail.com>
2023-11-08 02:34:40 +0500

    Edit article. UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom"
-import { Main, Login, Register, Navbar, ArticleDetail, CreateArticle } from './components'
+import { Main, Login, Register, Navbar, ArticleDetail, CreateArticle, EditArticle } from './components'
 import AuthService from "./services/auth"
 import { useDispatch } from "react-redux"
 import { signUserSuccess } from "./slice/auth"
@@ -36,6 +36,7 @@ const App = () => {
           <Route path="/register" element={<Register />} />
           <Route path="/article/:slug" element={<ArticleDetail />} />
           <Route path="/create-article" element={<CreateArticle />} />
+          <Route path="/edit-article/:slug" element={<EditArticle />} />
 
 
         </Routes>
